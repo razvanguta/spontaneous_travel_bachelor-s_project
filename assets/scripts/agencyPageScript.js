1cf9ce0b70a9cfcb358d7de49390ae2e8495d3a8
razvanguta <razvanguta20101999@gmail.com>
2021-12-18 23:01:06 +0200

    add script in proper page

--- assets/scripts/agencyPageScript.js
+++ assets/scripts/agencyPageScript.js
@@ -1,7 +1,11 @@
-fetch("/jsonAgency/"+"Turism1")
+console.log(document.getElementById("theUsername").innerHTML)
+fetch("/jsonAgency/"+ document.getElementById("theUsername").innerHTML)
 .then(response => response.json())
 .then(data => {
-    document.getElementById("username").innerHTML = "Bine ati venit pe pagina agentiei de turism" + data.username;
+    document.getElementById("username").innerHTML = "Bine ati venit pe pagina agentiei de turism " + data.username;
     document.getElementById("description").innerHTML =  data.description;
     document.getElementById("email").innerHTML = "Pentru mai multe detalii puteti contacta agentia prin email la adresa: " + data.email;
-})
+    var el = document.getElementById("image");
+    console.log(data.profile_image)
+    el.innerHTML = "<img src='../assets/images/"+data.profile_image+"'>"
+}) 
